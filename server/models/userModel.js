d88da44041cb8ba461ac40855211fa4cca8cf452
Nam <nam.nguyen@getcerebral.com>
2023-03-30 10:11:27 +0700

    register api

---
+++ server/models/userModel.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+// name, email, password, password, confirmPassword
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell us your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please tell us your email address"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please tell us your password"],
+    // Password must be at least 8 characters long
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+    },
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  // Only run this function if passowrd was modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
