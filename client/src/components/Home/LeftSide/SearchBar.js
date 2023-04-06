a97eef305fb3c302125ae2e2a805036a02c5aaa7
s3852307@rmit.edu.vn <s3852307@rmit.edu.vn>
2023-04-06 22:08:15 +0700

    Create side bar for home page

---
+++ client/src/components/Home/LeftSide/SearchBar.js
@@ -0,0 +1,32 @@
+import { Container, InputAdornment, TextField, Grid } from "@mui/material";
+import { useState } from "react";
+import search from './search.png';
+
+
+export default function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (event) => {
+  setSearchTerm(event.target.value);
+  };
+
+  return (
+      <Container>
+        <TextField 
+          id="search"
+          type="search"
+          label="Search"
+          value={searchTerm}
+          onChange={handleChange}
+          sx={{ width: 299 }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                  <img src={search} height={25} width={25}/>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Container>
+  );
+}
