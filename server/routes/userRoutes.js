7daf6e278075afa630f693eff4c800f89a19078a
Nam Nguyen <namnguyen20999@gmail.com>
2023-05-01 03:01:01 +0700

    Adding reset password

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,4 +6,9 @@ const router = express.Router();
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
+router.post("/forgotPassword", authController.forgotPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
+
+router.patch('/updateMyPassword',authController.protect, authController.updatePassword);
+
 module.exports = router;
